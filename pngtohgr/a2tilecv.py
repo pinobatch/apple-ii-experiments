ad93c8841f8b72d0f97fd8a99dcd08d94f907516
Damian Yerrick <pino@pineight.com>
2022-01-23 14:17:37 -0500

    a2tilecv: convert PNG to HGR
    was able to convert a sprite sheet and a photograph
    converting to tile-size chunks comes next

--- pngtohgr/a2tilecv.py
+++ pngtohgr/a2tilecv.py
@@ -9,6 +9,10 @@ GREEN = (77, 166, 64)
 BLUE = (77, 141, 191)
 ORANGE = (178, 114, 64)
 
+def hbascalc(y):
+    """Calculate the base address of each scanline of an HGR screen"""
+    return (y & 0x07) * 1024 + (y & 0x38) * 16 + (y >> 6) * 40
+
 def parse_argv(argv):
     p = argparse.ArgumentParser()
     p.add_argument("image")
@@ -47,18 +51,50 @@ def main(argv=None):
     rangelen = range(len(residue_levels))
     attrs = bytes(min(rangelen, key=row.__getitem__)
                   for row in zip(*residue_levels))
-    print(attrs.hex())
-    masks = [
-        bytes(255 if a == target else 0 for a in attrs)
-        for target in rangelen
-    ]
-    for m in masks:
-        print(m.hex())
+
+    # Paste them together for preview
+    combined = Image.new('P', im.size)
+    combopalette = [c for p in palettes for c in p]
+    combopalette.extend([BLACK] * (256 - len(combopalette)))
+    combopalette = bytes(level for rgb in combopalette for level in rgb)
+    combined.putpalette(combopalette)
+    attrsize = trials[0][2].size
+    for subpalid, (result, _, _) in enumerate(trials):
+        maskdata = bytes(255 if a == subpalid else 0 for a in attrs)
+        maskim = Image.new("L", attrsize)
+        maskim.putdata(maskdata)
+        maskim = maskim.resize(im.size, Image.NEAREST)
+        subpalbase = subpalid * len(palettes[0])
+        ptlut = bytes(range(subpalbase, subpalbase + len(palettes[0])))
+        ptlut += bytes(256 - len(ptlut))
+        combined.paste(result.point(ptlut, "P"), mask=maskim)
+    maskdata = maskim = subpalbase = ptlut = None
+
+    # Convert to a linear-order bitmap with (w, h) = attrsize
+    # Sample bits 1, 0, 1, 0, 1, 0, ... of each pixel
+    # (assuming width in pixels is a multiple of 14)
+    pixels = combined.tobytes()
+    bits = iter((b >> (~i & 1) & 1) for i, b in enumerate(pixels))
+    phase = 1
+    linearbits = bytearray()
+    for attr in attrs:
+        bytevalue = attr << 7
+        for bshift in range(7):
+            bytevalue |= next(bits) << bshift
+            phase ^= 1
+        linearbits.append(bytevalue)
+
+    # let's stuff it into an HGR file for viewing
+    hgrout = bytearray(8192)
+    for y in range(attrsize[1]):
+        srcoffset, dstoffset = attrsize[0] * y, hbascalc(y)
+        hgrout[dstoffset:dstoffset + attrsize[0]] = linearbits[srcoffset:srcoffset + attrsize[0]]
+    with open("a2tilecv.hgr", "wb") as outfp:
+        outfp.write(hgrout)
+        
+    
 
     # next steps:
-    # stuff each mask into an image and scale it to full size
-    # use point() to add the subpalette base to each trial result
-    # use paste(trialplusbase, mask=scaledmask) to build an overall image
     # convert bit 1 of even columns and bit 0 of odd columns to bytes
     # stuff this in an HGR file for viewing
 
